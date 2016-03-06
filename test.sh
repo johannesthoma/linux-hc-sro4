afb0669a21c4660715ab3987f79f55b7120fa377
Johannes Thoma <johannes@johannesthoma.com>
2016-03-06 19:55:23 +0000

    Updated test script: ignore errors and busy loop.
    So we can run n test scripts in paralell.

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 while true
 do
-	dist=`cat /sys/class/distance-sensor/distance_23_24/measure`
-        cm=$[ $dist * 17150 ]
-	printf "%d.%06d cm\n" $[ $cm/1000000 ] $[ $cm%1000000 ]
+	dist=`cat /sys/class/distance-sensor/distance_23_24/measure 2>/dev/null`
+ 	if [ "$dist" ] ; then
+	        cm=$[ $dist * 17150 ]
+		printf "%d.%06d cm\n" $[ $cm/1000000 ] $[ $cm%1000000 ]
+	fi
 #	sleep 1
 done
